d045b612066a798a8eee546f67048b1f377a08cb
Georgios Chaniotakis <haniotinho@gmail.com>
2020-04-26 03:32:54 +0300

    create fetch post action

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchPost} from '../actions';
 
 class PostList extends React.Component{
+    componentDidMount(){
+        this.props.fetchPost();
+    }
     render(){
         return<div>Post List</div>
     }
 }
 
-export default PostList;
+
+
+export default connect(null,{fetchPost})(PostList);
