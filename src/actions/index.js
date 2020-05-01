5bdf6b926e7697fd1d374bb2de976cc239028588
Georgios Chaniotakis <haniotinho@gmail.com>
2020-05-01 20:11:15 +0300

    memoizing fetch user action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
 
 export const fetchPost = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
@@ -6,8 +7,13 @@ export const fetchPost = () => async dispatch => {
     dispatch({type: 'FETCH_POSTS', payload: response.data})
 };
 
-export const fetchUser = (id) => async dispatch =>{
+export const fetchUser = (id) =>  dispatch =>{
+    _fetchUser(id, dispatch);
+};
+
+
+const _fetchUser = _.memoize(async (id, dispatch) =>{
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({type: 'FETCH_USER', payload: response.data});
-};
+});
